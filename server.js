9f9a6165d8b4d7645ff22228a477d0a10f2f19c1
Schwynn Sant <sschwynn@gmail.com>
2024-03-02 13:49:12 +0700

    Add Connection String

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
+const dentists = require("./routes/dentists");
+const { connect } = require("mongoose");
+const connectDB = require("./config/db");
 
-dotenv.config({path:'./config/config.env'});
+dotenv.config({ path: "./config/config.env" });
 
-const app=express();
+//connect to Mongo
+connectDB();
 
-const dentists = require('./routes/dentists');
+const app = express();
 
-app.use('/api/v1/dentists',dentists);
+app.use("/api/v1/dentists", dentists);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port', PORT));
+app.listen(
+  PORT,
+  console.log("Server running in ", process.env.NODE_ENV, " mode on port", PORT)
+);
