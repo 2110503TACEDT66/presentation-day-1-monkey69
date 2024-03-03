56f3d87f8536e701b7af297342dbd1110a451743
Kankawin Somrit <inwklong@gmail.com>
2024-03-04 00:04:37 +0700

    add logout

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {login, register, getMe} = require('../controllers/auth');
+const {login, register, getMe, logout} = require('../controllers/auth');
 const {protect} = require('../middleware/auth');
 const router = express.Router();
 const bookingRouter = require('./bookings');
@@ -9,5 +9,6 @@ router.use('/:authId/bookings',bookingRouter);
 router.post('/register',register);
 router.post('/login',login);
 router.get('/me',protect,getMe);
+router.get('/logout',logout);
 
 module.exports = router;
