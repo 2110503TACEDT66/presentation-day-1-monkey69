817e95d404b370fa18fe2be916a31b68088bef89
Kankawin Somrit <inwklong@gmail.com>
2024-03-03 10:11:18 +0700

    add bookingRouter to auth

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,9 @@ const express = require('express');
 const {login, register, getMe} = require('../controllers/auth');
 const {protect} = require('../middleware/auth');
 const router = express.Router();
+const bookingRouter = require('./bookings');
+
+router.use('/:authId/bookings',bookingRouter);
 
 router.post('/register',register);
 router.post('/login',login);
